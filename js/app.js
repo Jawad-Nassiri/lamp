c5b51f3697b83e5fa04a4c5474fdfdf9d690e4de
Jawad <jawadnasiri2222@gmail.com>
2025-07-14 12:47:59 +0200

    add javascript fuctionality

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const btnElement = document.querySelector('.button')
+const imgElement = document.querySelector('img')
+
+
+btnElement.addEventListener('click', () => {
+    imgElement.src = './pics/bulbon.gif'
+})
