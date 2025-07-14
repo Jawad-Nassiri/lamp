8573368a75d1efdf82eabc508709f13512766cfa
Jawad <jawadnasiri2222@gmail.com>
2025-07-14 12:58:39 +0200

    add toggle switch functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const btnElement = document.querySelector('.button')
 const imgElement = document.querySelector('img')
 
+let isOn = false
 
 btnElement.addEventListener('click', () => {
-    imgElement.src = './pics/bulbon.gif'
+    isOn = !isOn
+
+    if(isOn) {
+        imgElement.src = './pics/bulbon.gif'
+        btnElement.textContent = 'Turn off the light'
+    } else {
+        imgElement.src = './pics/bulboff.gif'
+        btnElement.textContent = 'Turn on the light'
+    }
 })
